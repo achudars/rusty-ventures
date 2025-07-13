b2f16314df89cf5ae8465d3cf68d6f39bd1f5017
aleks <achudars@gmail.com>
2025-07-13 22:58:47 +0100

    feat: update favicon to burgundy red (#800020) square
    - Replace default favicon with burgundy red square using #800020 color
    - Create favicon.ico, favicon.png, and favicon.svg versions
    - Backup original favicon as favicon-old.ico
    - Update _document.tsx with proper favicon link tags
    - Favicon matches the burgundy color from Tailwind config
    - Optimized file sizes: 131 bytes for PNG/ICO, 154 bytes for SVG

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Burgundy favicon */}
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon.png" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
