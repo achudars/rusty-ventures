e33efebdc956d6dfb8e69f0adb5cca4883917f9c
aleks <achudars@gmail.com>
2026-05-31 20:14:36 +0100

    chore: ignore rust/target build artifacts, bump to v0.2.1
    - Add rust/target/ to .gitignore (Rust build cache should not be tracked)
    - Bump package.json version 0.2.0 -> 0.2.1 to tag the wasm fix release

--- rust/target/wasm32-unknown-unknown/release/deps/rust.d
+++
@@ -1,5 +0,0 @@
-C:\Users\aleks\github\javascript\rusty-ventures\rust\target\wasm32-unknown-unknown\release\deps\rust.wasm: src\lib.rs
-
-C:\Users\aleks\github\javascript\rusty-ventures\rust\target\wasm32-unknown-unknown\release\deps\rust.d: src\lib.rs
-
-src\lib.rs:
--- rust/target/wasm32-unknown-unknown/release/rust.d
+++
@@ -1,1 +0,0 @@
-C:\Users\aleks\github\javascript\rusty-ventures\rust\target\wasm32-unknown-unknown\release\rust.wasm: C:\Users\aleks\github\javascript\rusty-ventures\rust\src\lib.rs
